411e6c90523babca969739ae59dca854d66908e6
kiseokkm <311kskim@gmail.com>
2024-05-13 16:36:58 +0900

    🎉 add : set

---
+++ src/main/java/com/sparta/nbcamp_java_5th_springprepare/NbcampJava5thSpringPrepareApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.nbcamp_java_5th_springprepare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbcampJava5thSpringPrepareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NbcampJava5thSpringPrepareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/nbcamp_java_5th_springprepare/NbcampJava5thSpringPrepareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.nbcamp_java_5th_springprepare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbcampJava5thSpringPrepareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
